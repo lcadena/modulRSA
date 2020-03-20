0ff848ed55ffd9a9c51705e343d472516cfad06e
AnnaDiazPineda <31926227+AnnaDiazPineda@users.noreply.github.com>
2020-03-20 14:16:11 +0100

    ...

---
+++ src/EncryptDecryptClass.js
@@ -0,0 +1,41 @@
+import java.math.BigInt;
+import java.util.Scanner;
+
+
+Public class PublicKey
+{
+  constructor( e, N) {
+    this.e = BigInt e;
+    this.N = BigInt N;
+  }
+}
+
+Public class PrivateKey
+{
+    constructor( d, n)
+        this.d = BigInt d;
+        this.n = BigInt n;
+    }
+
+
+
+   function  encrypt ( m, n){
+
+        var this.n              = n;
+        const this.m            = BigInt m;
+        const                   = BigInt math.pow(m,e)%n
+        console.log(operation);
+
+        return operation;
+
+    }
+
+    function  decrypt (BigInt cripto){
+
+            bigint  m = this.m;
+            bigint  operation = math.pow(c,d)%n
+                        console.log(operation);
+
+            return operation;
+    }
+
